5df14edab21b92325aeaf338d9c3e1d73bf8b31a
Victor Nguyen <victor.bt.nguyen@gmail.com>
2022-07-28 15:22:07 -0500

    Update WordCloudRunner.java

--- src/WordCloudRunner.java
+++ src/WordCloudRunner.java
@@ -3,8 +3,6 @@
 
 public class WordCloudRunner {
 	
-	//enters in a url and uh dabs on it very hard until a picture comes out
-	//might take like 20 minutes and 20% of cpu due to constipation
 	public static void main(String[] args) throws Exception {
 		WordCloud cloud = new WordCloud("https://www.cfisd.net/en");
 		WordCloudDisplay display = new WordCloudDisplay(cloud, 800, 600, 250, "CFISD's Main Website");
